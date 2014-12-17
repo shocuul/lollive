f8ec08704e9f039db36387d7b01b224d084c95ee
Shocuul <shocuul@live.com>
2014-12-17 12:46:07 -0600

    Initial Commit

---
+++ League of Legend LiveTests/League_of_Legend_LiveTests.swift
@@ -0,0 +1,36 @@
+//
+//  League_of_Legend_LiveTests.swift
+//  League of Legend LiveTests
+//
+//  Created by NeoPolitica TV on 17/12/14.
+//  Copyright (c) 2014 Denethiel Studio. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class League_of_Legend_LiveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
